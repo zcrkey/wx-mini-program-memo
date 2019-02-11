e7a86e3814bdd743438fbb3cce3777fcbb7e9752
zkey <774508556@qq.com>
2019-02-11 17:48:04 +0800

    关于

---
+++ SourceCode/memo/pages/about/about.js
@@ -0,0 +1,5 @@
+"use strict";
+Page({
+    data: {}
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYWJvdXQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJhYm91dC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQ0EsSUFBSSxDQUFDO0lBQ0gsSUFBSSxFQUFFLEVBQ0w7Q0FDRixDQUFDLENBQUEiLCJzb3VyY2VzQ29udGVudCI6WyIvL2Fib3V0LmpzXHJcblBhZ2Uoe1xyXG4gIGRhdGE6IHtcclxuICB9XHJcbn0pIl19
---
+++ SourceCode/memo/pages/about/about.ts
@@ -0,0 +1,5 @@
+//about.js
+Page({
+  data: {
+  }
+})
