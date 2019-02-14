9e258f64983d644a9017f7538a6acbbcb0599ad9
zkey <774508556@qq.com>
2019-02-14 11:32:55 +0800

    添加免责声明

--- SourceCode/memo/pages/about/about.ts
+++ SourceCode/memo/pages/about/about.ts
@@ -25,5 +25,17 @@ Page({
       // 设置编辑状态
       wx.setStorageSync('isEdit', false)
     } catch (e) { }
+  },
+
+  /**
+   * 免责声明
+   */
+  onClickDisclaimer() {
+    wx.showModal({
+      title: '免责声明',
+      content: '软件仅供技术交流，请勿用于商业及非法用途，如产生法律纠纷与本人无关!',
+      showCancel: false
+    })
   }
+
 })
